d81099be9f5a89ec8ebf6df6d71aab609b94c450
Khoa Tran <khoa.d.tran97@gmail.com>
2019-04-29 16:28:55 -0700

    buggy code

--- mp3.cpp
+++ mp3.cpp
@@ -1,3 +1,27 @@
+#include <cstdint>
+// #include <project_config.hpp>
+// #include <iterator>
+// #include "utility/log.hpp"
+// #include "utility/time.hpp"
+// #include "L0_LowLevel/LPC40xx.h"
+// #include "L1_Drivers/pin.hpp"
+// #include "utility/enum.hpp"
+// #include "L0_LowLevel/interrupt.hpp"
+#include <stdint.h>
+#include <stdio.h>
+// // #include "task.h"
+// // #include "queue.h"
+// #include "L2_HAL/switches/button.hpp"
+// #include "L3_Application/oled_terminal.hpp"
+// #include "vector"
+#include "third_party/fatfs/source/ff.h"
+#include "labspi.hpp"
+#include "labgpio.hpp"
+
+LabGPIO xdcs(2,0);  // VS1053 Data Select
+LabGPIO dreq(2,2);  // VS1053 Data Request Interrupt Pin
+LabGPIO mp3cs(2,5); // VS1053 Chip Select
+LabGPIO sdcs(2,7);  // SD Card Chip Select
 
 class decoder{
 public:
@@ -6,7 +30,7 @@ public:
 	}
 
 	void init(){
-		spi.Initialize();
+		spi.Initialize(8, LabSpi::FrameModes::spi, 0);
 
 		dreq.SetAsInput();
 		xdcs.SetAsOutput();
@@ -15,19 +39,19 @@ public:
 
 		mp3cs.SetLow();//chip select enable in init
 		sdcs.SetHigh();
-		xdcs.SetHigh();
-	}
-
-	void data_enable(){
 		xdcs.SetLow();
 	}
 
-	void data_disable(){
+	void data_enable(){
 		xdcs.SetHigh();
 	}
 
+	void data_disable(){
+		xdcs.SetLow();
+	}
+
 	void send(uint8_t data){
-		spi.transfer(data);
+		spi.Transfer(data);
 	}
 
 	bool check(){
@@ -38,10 +62,10 @@ public:
 
 private:
 	LabSpi spi;
-	LabGPIO xdcs(2,0);  // VS1053 Data Select
-	LabGPIO dreq(2,2);  // VS1053 Data Request Interrupt Pin
-	LabGPIO mp3cs(2,5); // VS1053 Chip Select
-	LabGPIO sdcs(2,7);  // SD Card Chip Select
+	// LabGPIO xdcs(2,0);  // VS1053 Data Select
+	// LabGPIO dreq(2,2);  // VS1053 Data Request Interrupt Pin
+	// LabGPIO mp3cs(2,5); // VS1053 Chip Select
+	// LabGPIO sdcs(2,7);  // SD Card Chip Select
 };
 
 bool isMP3(char* file_name){
@@ -63,54 +87,67 @@ FILINFO fno;
 
 
 int main(){
-	char song_name[20][20];
+	char song_name[20][13];
 	int song_count=0;
 
 	//mounting and open root directory
 	f_mount(&fat_fs,"",1);
-	FRESULT res = f_opendir(&dir,"/");
+	// FRESULT res = f_opendir(&dir,"/");
 	
 	//scan sd card and store song names in buffer
-	if(res == FR_OK){
-		for(;;){
-			res = f_readdir(&dir,&fno);
-			if(res != FR_OK || fno.fname[0] == 0) break;//error or end of loop
-			else if(fno.fattrib & AM_DIR){
-				//a folder, ignore
-			}
-			else{
-				printf("%s\n", fno.fname);//print file name for testing
-				if(isMP3(fno.fname)){
-					songname[song_count] = fno.fname;
-					if(++song_count >= 20){
-						song_count = 19;
-						printf("can only handle 20 songs\n");
-					}
-				}
-			}
-		}
-	}
+	// if(res == FR_OK){
+	// 	for(;;){
+	// 		res = f_readdir(&dir,&fno);
+	// 		if(res != FR_OK || fno.fname[0] == 0) break;//error or end of loop
+	// 		else if(fno.fattrib & AM_DIR){
+	// 			//a folder, ignore
+	// 		}
+	// 		else{
+	// 			printf("%s\n", fno.fname);//print file name for testing
+	// 			if(isMP3(fno.fname)){
+	// 				printf("%s\n",fno.fname);
+	// 				// song_name[song_count] = static_cast<char>(fno.fname);
+	// 				// if(++song_count >= 20){
+	// 				// 	song_count = 19;
+	// 				// 	printf("can only handle 20 songs\n");
+	// 				// }
+	// 			}
+	// 		}
+	// 	}
+	// }
 
 	//send a mp3 file to decoder for testing
-	UINT file_size;
-	uint8_t data_buffer[10000000];//could be change if not enough to fit the file
-	f_open(&fil,"file_name.mp3",FA_READ,10000000,&file_size);
+	// UINT file_size;
+	// uint8_t data_buffer[10000];//could be change if not enough to fit the file
+	// f_open(&fil,"four4.mp3",FA_READ);
+	// f_read(&fil,data_buffer,10000,&file_size);
+	// printf("files ize: ");
+	// printf("%i",file_size);
 
-	decoder decode;
-	decode.init();
+	//decoder decode;
+	LabSpi decode;
+	decode.Initialize((uint8_t)8,LabSpi::FrameModes::spi,(uint8_t)0);
+	//decode.init();
 	int i=0;
 
-	decode.data_enable();
+	decode.Transfer(0x88);
+	decode.Transfer(0xA8);
+	decode.Transfer(0xB8);
+	while(1){}
+	// decode.data_enable();
 
-	while(i<file_size){//check for eof
-		if(decode.check()){
-			for(int j=0;j<32;j++){
-				if(i<file_size){//check again for eof
-					decode.transfer(data_buffer[i++]);
-				}
-			}
-		}
-	}
+	// while(i<file_size){//check for eof
+	// 	if(decode.check()){
+	// 		decode.data_enable();
+	// 		for(int j=0;j<32;j++){
+	// 			if(i<file_size){//check again for eof
+	// 				// printf("%x\n",data_buffer[i]);
+	// 				decode.send(data_buffer[i++]);
+	// 			}
+	// 		}
+	// 		decode.data_disable();
+	// 	}
+	// }
 
 
 	return 0;
